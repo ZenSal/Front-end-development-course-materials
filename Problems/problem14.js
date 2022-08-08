9ad5a38949491bfce5b0127098682ed9a478c509
ZenSal <magesh.murugesan08@gmail.com>
2022-08-08 18:36:13 +0530

    Intial commit

---
+++ Problems/problem14.js
@@ -0,0 +1,14 @@
+
+
+function subarray(){
+    for(i=0; i<array.length-1; i++){
+        if(array[i]<array[i+1]){
+            let result = array[i];
+        }else if(array[i]>array[i+1]){
+            
+        }
+    }
+
+}
+
+subarray()
