679cbb61260666e8bccc0e2ff459da91f0121305
ZenSal <magesh.murugesan08@gmail.com>
2022-08-10 18:36:56 +0530

    commit 2

--- Problems/problem14.js
+++ Problems/problem14.js
@@ -1,14 +1,43 @@
 
+function subarraySort(array){
+    //if(array.length==0){
+        //return true;
+    //}
+    let startex;
+    let endex;
+    let decreasingArray=[];
+    for(let i=0;i<array.length;i++){
+        
+        if(array[i]>array[i+1]){
+            startex=i;
+            break;
 
-function subarray(){
-    for(i=0; i<array.length-1; i++){
-        if(array[i]<array[i+1]){
-            let result = array[i];
-        }else if(array[i]>array[i+1]){
-            
+        }
+    }
+    for(i=startex; i<array.length; i++){
+        
+        if(array[i]<array[i+1] || i==array.length-1){
+            endex=i;
+            break;
         }
     }
 
+    for(let i=startex; i<=endex; i++){
+        
+        decreasingArray.push(array[i]);
+    }
+    //console.log(decreasingArray);
+
+    for(let i=decreasingArray.length-1; i>=0; i--){
+        array[startex]=decreasingArray[i];
+        subarraySort(array);
+        startex++;
+
+    }
+    console.log(array);
+
 }
 
-subarray()
+//subarraySort([1,2,5,4,3]);
+
+subarraySort([1,2,3,4,6,5,7,8,9,12,11,10]);
---
+++ Problems/problem15.js
@@ -0,0 +1,15 @@
+function PrimeNum(n){
+    for(let i=3; i<=n;i++){
+        let j=2;
+        if(i%j !==0){
+            console.log(i);
+            j++;
+            
+
+            }
+
+        }
+    }
+PrimeNum(20);
+//PrimeNum(7);
+
